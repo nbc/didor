ff775a9cc08e93a69977d51fe2452dd044ec1d4f
nbc <nicolas@barna.be>
2021-11-29 22:47:58 +0100

    work on tests

--- tests/testthat/test-get_attachments.R
+++ tests/testthat/test-get_attachments.R
@@ -1,9 +1,21 @@
 get_metadata()
 
+test_that("check fail if arg isn't a dataframe ", {
+  data = c("a", "b")
+  expect_error(get_attachments(data), "must be a dataframe")
+})
+
+test_that("check fail if arg isn't a dataframe ", {
+  data <- tibble(
+    name = c("a", "b")
+  )
+  expect_error(get_attachments(data), "data must have")
+})
+
 test_that("check get_attachments", {
   skip_on_cran()
   skip_if_offline()
   urls <- datasets() %>% attachments() %>% select(url) %>% slice(1)
 
-  expect_message(get_attachments(urls), "downloaded", dest = tempdir())
+  expect_message(get_attachments(urls, dest = tempdir()), "downloaded")
 })
---
+++ tests/testthat/test-get_data.R
@@ -0,0 +1,33 @@
+
+test_that("get_data work with last_millesime input", {
+  skip_on_cran()
+  skip_if_offline()
+
+  get_metadata()
+
+  data <- datasets() %>% datafiles() %>% last_millesime() %>% arrange(rows) %>% slice_head()
+  nb_of_rows <- data[["rows"]]
+  result <- get_data(data)
+
+  expect_equal(nb_of_rows, nrow(result))
+})
+
+test_that("get_data work with datafiles input", {
+  skip_on_cran()
+  skip_if_offline()
+
+  get_metadata()
+
+  data <- datasets() %>% datafiles() %>% last_millesime() %>% arrange(rows) %>% slice_head()
+  nb_of_rows <- data[["rows"]]
+  result <- get_data(data %>% select(rid))
+
+  expect_equal(nb_of_rows, nrow(result))
+})
+
+
+test_that("get_data fail", {
+  expect_error(get_data(), "is mandatory")
+  expect_error(get_data(c("a")), "must be a dataframe")
+  expect_error(get_data(tibble(name = c(1, 2))), "must have an")
+})
